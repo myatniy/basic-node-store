c26094ff1740dad3dfa7568dbe0e1bfcfd7fb2a7
myatniy <ipsixin3d@gmail.com>
2020-08-01 16:43:11 +0300

    Refactor

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
-const { dirname } = require("path");
+const { createServer } = require("http");
+const { readFileSync } = require("fs");
+const { parse } = require("url");
+const replaceTemplate =  require(`${__dirname}/my-modules/replaceTemplate`);
 const log = console.log;
 
-const data = fs.readFileSync(`${__dirname}/data.json`, "utf-8");
+const data = readFileSync(`${__dirname}/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 const htmlTemplates = {
-    card: fs.readFileSync(`${__dirname}/templates/card.html`, "utf-8"),
-    overview: fs.readFileSync(`${__dirname}/templates/overview.html`, "utf-8"),
-    product: fs.readFileSync(`${__dirname}/templates/product.html`, "utf-8")
+    card: readFileSync(`${__dirname}/templates/card.html`, "utf-8"),
+    overview: readFileSync(`${__dirname}/templates/overview.html`, "utf-8"),
+    product: readFileSync(`${__dirname}/templates/product.html`, "utf-8")
 }
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
-    output = output.replace(/{%ID%}/g, product.id);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-    return output;
-};
-
-const server = http.createServer((req, res) => {
-    const {query, pathname} = url.parse(req.url, true);
+const server = createServer((req, res) => {
+    const {query, pathname} = parse(req.url, true);
 
     /* Basic router */
     // api
